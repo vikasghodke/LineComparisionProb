6f0e2d24c2e698120a00f54051790efac4b953e5
vikas ghodke <vikasghodke468@gmail.com>
2023-12-23 17:59:10 +0530

    second commit

--- UseCases.java
+++ UseCases.java
@@ -7,6 +7,24 @@ public class UseCases {
 		System.out.println("Welcome to lince comparision competition program On Master Branch");
 		
 		
+		Scanner in=new Scanner(System.in);
+				
+				System.out.println("Enter a x1");
+				double x1=in.nextInt();
+				System.out.println("Enter a y1");
+				double y1=in.nextInt();
+				
+				System.out.println("Enter a x2");
+				double x2=in.nextInt();
+				
+				System.out.println("Enter a y2");
+				double y2=in.nextInt();
+				
+				double distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+				
+				System.out.println("distance="+distance);
+		
+		
 		
 		
 		
