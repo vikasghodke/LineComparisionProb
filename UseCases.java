c1e0358c6f94c25fed1d42a8854efffef5257422
vikas ghodke <vikasghodke468@gmail.com>
2023-12-23 17:57:21 +0530

    first commit

---
+++ UseCases.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class UseCases {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to lince comparision competition program On Master Branch");
+		
+		
+		
+		
+		
+		
+		
+
+    
+	
+    
+   
+		
+		
+    }
+
+	
+	}
+
+	
+	
+
+
